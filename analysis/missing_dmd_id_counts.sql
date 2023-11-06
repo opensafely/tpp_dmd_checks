f05b24ddc7184a703dc4839edbd8c3663cd6c787
Jon Massey <jon.massey@thedatalab.org>
2023-11-06 11:22:10 +0000

    Add SQL query and project config

---
+++ analysis/missing_dmd_id_counts.sql
@@ -0,0 +1,39 @@
+-- Internal project for data curation, no need for PatientsWithTypeOneDissent
+
+;with
+    cte
+    as
+    (
+        SELECT
+            d.MultilexDrug_ID,
+            d.dmd_id,
+            d.FullName,
+            year(i.ConsultationDate) as [year],
+            cast(count(*) as float) as IssueCount
+        FROM MedicationIssue i
+            JOIN MedicationDictionary d
+            on i.MultilexDrug_ID = d.MultilexDrug_ID
+        WHERE 
+            ISNUMERIC(d.dmd_id) = 0
+            AND NOT EXISTS (
+                SELECT 1 
+                FROM OpenCoronaTempTables..CustomMedicationDictionary c 
+                WHERE c.MultilexDrug_ID = d.MultilexDrug_ID
+            )
+            AND year(i.ConsultationDate)>=2018
+        GROUP BY
+            d.MultilexDrug_ID,
+            d.dmd_id,
+            d.FullName,
+            year(i.ConsultationDate),
+            datepart(q,i.ConsultationDate)
+    )
+
+select
+    MultilexDrug_ID,
+    dmd_id,
+    FullName,
+    [Year],
+    cast(CASE WHEN IssueCount=0 THEN 0 ELSE (CEILING(IssueCount/6)*6) - 3 END as int) as IssueCount_midpoint6
+from cte
+order by 1,2,3,4,5
